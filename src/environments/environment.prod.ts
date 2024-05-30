529786f2676c294cdb62b100290d9307e4cf647d
ccrismota <ccrismota@gmail.com>
2024-05-30 13:14:15 -0300

    feat:mudança de conta do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,19 @@
 export const environment = {
   firebase: {
-    projectId: 'app-diario-viagens',
-    appId: '1:737202778494:web:6bec720cdbaa535f5e432f',
-    storageBucket: 'app-diario-viagens.appspot.com',
+    // projectId: 'app-diario-viagens',
+    // appId: '1:737202778494:web:6bec720cdbaa535f5e432f',
+    // storageBucket: 'app-diario-viagens.appspot.com',
+    // locationId: 'us-central',
+    // apiKey: 'AIzaSyCGYErbaQ3T3cWxjhbz_7yJdHWkhFDvHtQ',
+    // authDomain: 'app-diario-viagens.firebaseapp.com',
+    // messagingSenderId: '737202778494',
+    apiKey: "AIzaSyDGO1JSVrHwZosZ7S6HuV6Yd76FRmvp1cs",
+    authDomain: "diario-viagens-a160f.firebaseapp.com",
+    projectId: "diario-viagens-a160f",
+    storageBucket: "diario-viagens-a160f.appspot.com",
     locationId: 'us-central',
-    apiKey: 'AIzaSyCGYErbaQ3T3cWxjhbz_7yJdHWkhFDvHtQ',
-    authDomain: 'app-diario-viagens.firebaseapp.com',
-    messagingSenderId: '737202778494',
+    messagingSenderId: "592237195575",
+    appId: "1:592237195575:web:3382c03e7666722aa02ab2",
   },
   production: true
 };
